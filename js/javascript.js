7918b0322c6e3ed13d72844e1b618a14f2a3e109
Pinter Balint <pinter.balint06programming@gmail.com>
2023-03-09 18:41:34 +0100

    kartyak atdolgozassa, befejezese es reszponzivitas szimulator

--- js/javascript.js
+++ js/javascript.js
@@ -1,51 +1,65 @@
-const myCarouselElement = document.querySelector('#telepites')
-
-const carousel = new bootstrap.Carousel(myCarouselElement, {
-  interval: 2000,
-  touch: true
-})
+function meretezhetoRatioSzamolas() {
+  var meretezhetoRatio = $('#meretezheto').width() / $('#meretezhetoparent').width();
+  if (meretezhetoRatio < 0.65) {
+    $('#meretezheto').children().each(function() {
+      $(this).css("width", "100%");
+    });
+  } else {
+    $('#meretezheto').children().each(function() {
+      $(this).css("width", "calc(100% / 3)");
+    });
+  }
+}
 
 $(document).ready(function () {
+  window.onresize = function () {
+    meretezhetoRatioSzamolas();
+  }
+  
+  meretezhetoRatioSzamolas();
+
+  $('#meretezheto').resizable({
+    handles: {
+      'e': '#meretezokar'
+    }
+  });
+
   $('.change-color').on('click', function () {
     var colorClass = $(this).attr('class').split(' ')[1].replace("btn-", "bg-");
     $('#color-box').removeClass().addClass('w-100 m-3 ' + colorClass);
   });
 
   $("#btn-col-3").click(function () {
-    $("#cols").children().fadeOut(500, function () {
+    $("#cols").children().slideUp(500, function () {
       $("#cols").empty();
-      $("#cols").append($("<div class='col-3 bg-danger'>1</div>"));
-      $("#cols").append($("<div class='col-3 bg-success'>2</div>"));
-      $("#cols").append($("<div class='col-3 bg-warning'>3</div>"));
-      $("#cols").append($("<div class='col-3 bg-primary'>4</div>"));
-      $("#cols").children().fadeIn(500);
+      $("<div class='col-3 bg-danger'></div>").appendTo($("#cols")).hide().slideDown(500);
+      $("<div class='col-3 bg-success'></div>").appendTo($("#cols")).hide().slideDown(500);
+      $("<div class='col-3 bg-warning'></div>").appendTo($("#cols")).hide().slideDown(500);
+      $("<div class='col-3 bg-primary'></div>").appendTo($("#cols")).hide().slideDown(500);
     });
   });
 
   $("#btn-col-4").click(function () {
-    $("#cols").children().fadeOut(200, function () {
+    $("#cols").children().slideUp(500, function () {
       $("#cols").empty();
-      $("#cols").append($("<div class='col-4 bg-danger'>1</div>"));
-      $("#cols").append($("<div class='col-4 bg-success'>2</div>"));
-      $("#cols").append($("<div class='col-4 bg-warning'>3</div>"));
-      $("#cols").children().fadeIn(200);
+      $("<div class='col-4 bg-danger'></div>").appendTo($("#cols")).hide().slideDown(500);
+      $("<div class='col-4 bg-success'></div>").appendTo($("#cols")).hide().slideDown(500);
+      $("<div class='col-4 bg-warning'></div>").appendTo($("#cols")).hide().slideDown(500);
     });
   });
 
   $("#btn-col-6").click(function () {
-    $("#cols").children().fadeOut(200, function () {
+    $("#cols").children().slideUp(500, function () {
       $("#cols").empty();
-      $("#cols").append($("<div class='col-6 bg-danger'>1</div>"));
-      $("#cols").append($("<div class='col-6 bg-success'>2</div>"));
-      $("#cols").children().hide().fadeIn(200);
+      $("<div class='col-6 bg-danger'></div>").appendTo($("#cols")).hide().slideDown(500);
+      $("<div class='col-6 bg-success'></div>").appendTo($("#cols")).hide().slideDown(500);
     });
   });
 
   $("#btn-col-12").click(function () {
-    $("#cols").children().fadeOut(200, function () {
+    $("#cols").children().slideUp(500, function () {
       $("#cols").empty();
-      $("#cols").append($("<div class='col-12 bg-danger'>1</div>"));
-      $("#cols").children().hide().fadeIn(200);
+      $("<div class='col-12 bg-danger'></div>").appendTo($("#cols")).hide().slideDown(500);
     });
   });
 });
